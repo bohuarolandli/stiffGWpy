1fac1090aa5cef5408264ae3e9496f7ff9b8af53
Bohua Li <rolandliholmes@gmail.com>
2023-07-17 02:27:09 +0800

    Major update:
    1. New model parameters: kappa10 and f_end
    2. NANOGrav 15yr likelihood

---
+++ cobaya/likelihoods/PTA/NANOGrav.py
@@ -0,0 +1,80 @@
+from cobaya.likelihood import Likelihood
+from cobaya.log import LoggedError, get_logger
+import numpy as np
+import os
+from scipy import interpolate
+import astropy.units as u
+#from scipy.stats import gaussian_kde as kde
+
+class NANOGrav(Likelihood):
+    
+    def initialize(self):
+        """
+        Initializes the class (called from __init__, before other initializations).
+        Prepare any computation, importing any necessary code, files, etc.
+        """
+        self.data = np.load(self.kde_file)                   # log-likelihood for each frequency bin
+        self.log10rhogrid = np.load(self.log10rhogrid_file)  # log10(delay/s) = log10(sqrt(rho/s^2))
+        self.freqs = np.load(self.freq_file)                 # Hz, frequency bins of the PTA experiment
+    
+    def close(self):
+        pass
+    
+            
+    def get_requirements(self):
+        """
+        return dictionary specifying quantities that are always needed and calculated by a theory code
+        """
+        return {'f': None, 'omGW_stiff': None, 'hubble': None}
+    
+    
+    def logp(self, _derived=None, **params_values):
+        """
+        The default implementation of the Likelihood class does the calculation in this 'logp()' function, 
+        which is called by 'Likelihood.calculate()' to save the log-likelihood into "state['logp']" 
+        (the latter may be more convenient if you also need to calculate some derived parameters).
+        
+        'logp()' can take a dictionary (as keyword arguments) of nuisance parameter values, 'params_values', 
+        (if there is any), and returns a log-likelihood.
+        """
+        f_theory = self.provider.get_result('f'); f_theory = np.flip(f_theory)                  # log10(f/Hz)
+        Ogw_theory = self.provider.get_result('omGW_stiff'); Ogw_theory = np.flip(Ogw_theory)   # log10(Omega_GW(f))
+        H_0 = self.provider.get_result('hubble')                                                # s^{-1}
+        
+        return self.log_likelihood(f_theory, Ogw_theory, H_0, **params_values)
+
+    
+    def log_likelihood(self, f_theory, Ogw_theory, H_0, **data_params):
+        """
+        where the calculation is actually done, independently of Cobaya
+        Here f_theory must be increasing.
+        """
+        yr = u.yr.to(u.s)               # s, one Julian year
+        T_base = 1/self.freqs[0]        # s, baseline of the PTA data
+        work_freqs = self.freqs[:14]    # work frequencies in Hz, i=1-14 for NG15
+        
+        # Primordial SGWB
+        cond = (f_theory >= -11) & (f_theory <= -5) 
+        f_t = f_theory[cond]; Ogw_t = Ogw_theory[cond]
+        
+        spec_prim = interpolate.CubicSpline(f_t, Ogw_t)
+        Ogw_prim = spec_prim(np.log10(work_freqs))        
+        rho_prim = np.divide(10**Ogw_prim * H_0**2, 8*np.pi**4 * work_freqs**5 * T_base)         # s^2
+
+        # SGWB from SMBHBs
+        log10hc_BH = data_params['A_BBH']      # log10(h_c) at f_yr
+        gamma_BH = data_params['gamma_BBH']
+        rho_BH = 10**(log10hc_BH*2) * (work_freqs*yr)**(-gamma_BH) * yr**3 / (12*np.pi**2 * T_base)  # s^2
+
+        
+        log10rho_Model = np.log10(np.sqrt(rho_prim + rho_BH))    # log10(delay/s) = log10(sqrt(rho/s^2))
+        #print(log10rho_Model)       
+        
+        logL = 0
+        for i in range(len(work_freqs)):
+            KDE_i = interpolate.CubicSpline(self.log10rhogrid, self.data[0,i])
+            logL += KDE_i(np.max([log10rho_Model[i], self.log10rhogrid[0]]))
+            # If rho_Model(f_i) is smaller than the lower limit of the given KDE range, ~ -15,
+            # use the lower limit to calculate the logpdf instead.
+
+        return logL
